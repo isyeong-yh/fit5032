cebb0377bf226133f606994191e72f04e2e2c112
isyeong-yh <yeongisaac@gmail.com>
2024-08-01 08:57:36 +1000

    Committing week 1 and 2 studio tasks

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>ID: 30610788</h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: 0px;
+}
+
+h3 {
+  font-weight: 500;
+  font-size: 2rem;
+  position: relative;
+  top: 0px;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
